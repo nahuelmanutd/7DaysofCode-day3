26ad26eb33b4658ef0b89da7b9ebd6647b259cbc
nahuelmanutd <161343338+nahuelmanutd@users.noreply.github.com>
2025-09-02 17:12:45 -0300

    Primer commit

---
+++ dia3.js
@@ -0,0 +1,38 @@
+let preguntaUno = "";
+
+let preguntaDos = "";
+
+function ifFrontend() {
+    preguntaUno = "Front-end";
+
+    console.log(`Pregunta uno: ${preguntaUno}`);
+}
+function ifBackend() {
+    preguntaUno = "Back-end";
+
+    console.log(`Pregunta uno: ${preguntaUno}`);
+}
+
+//If front-end:
+function reactFront() {
+    preguntaDos = "React";
+
+    console.log(`Pregunta dos: ${preguntaDos}`);
+}
+function vueFront() {
+    preguntaDos = "Vue";
+
+    console.log(`Pregunta dos: ${preguntaDos}`);
+}
+
+//If back-end:
+function cSharpBack() {
+    preguntaDos = "C#";
+
+    console.log(`Pregunta dos: ${preguntaDos}`);
+}
+function javaBack() {
+    preguntaDos = "Java";
+
+    console.log(`Pregunta dos: ${preguntaDos}`);
+}
