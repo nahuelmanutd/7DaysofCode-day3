36bf160a9b61c84a1c343de08d16e9cf4b6bed2c
Nahuel Tiseyra <nahueltiseyra158@gmail.com>
2025-09-02 21:31:09 -0300

    Funcionamiento completo de la página

--- dia3.js
+++ dia3.js
@@ -1,6 +1,7 @@
 let preguntaUno = "";
-
 let preguntaDos = "";
+let preguntaTres = "";
+let arrayTecno = [];
 
 function ifFrontend() {
     preguntaUno = "Front-end";
@@ -36,3 +37,40 @@ function javaBack() {
 
     console.log(`Pregunta dos: ${preguntaDos}`);
 }
+
+//if fullFront/Back.
+function fullFront() {
+    preguntaTres = "Seguir Front-end";
+
+    console.log(`Pregunta tres: ${preguntaTres}`);
+}
+function fullBack() {
+    preguntaTres = "Seguir Back-end";
+
+    console.log(`Pregunta tres: ${preguntaTres}`);
+}
+
+//if fullStack.
+function fullStack() {
+    preguntaTres = "Ser Full-stack";
+
+    console.log(`Pregunta tres: ${preguntaTres}`);
+}
+
+//Array tecnologías.
+function seguirTexto() {
+    let inputValue = document.getElementById("input").value;
+
+    if(inputValue == "") {
+        alert("Inserte un texto válido.");
+    } else {
+        arrayTecno.push(inputValue);
+        document.getElementById("input").value = "";
+    }
+    console.log(arrayTecno);
+}
+document.getElementById("input").addEventListener("keydown", function(event) { //Hace que, al presionar enter (enfocando el "input"), este active la función "seguirTexto()"
+    if(event.key === "Enter") {
+        seguirTexto();
+    }
+})
